f586ed27ed5c446dbc89416beb1881c217912119
Keir Simmons <keir.simmons11@alumni.imperial.ac.uk>
2022-11-16 02:31:17 +0900

    Add price searching in find

--- find.py
+++ find.py
@@ -3,29 +3,36 @@ import argparse
 from collection import Collection
 
 
-def find(cert):
+def find(cert, price):
     collection = Collection()
+
     print(f"Details for card of cert {cert}:")
-    print(collection.get(cert, replace=True))
+    data = collection.get(cert, replace=True)
+    print(data)
 
-    print("\nPossible equivalent cards:")
-    print(collection.find_dupes(cert))
+    if price:
+        print(f"To be sold at {data['selling']['price']} JPY")
+    else:
 
-    same, pkmn = collection.find_same_attr(cert, "pkmn")
-    print(f"\nCards with the same Pokemon ({pkmn}):")
-    print(same)
+        print("\nPossible equivalent cards:")
+        print(collection.find_dupes(cert))
 
-    same = collection.find_same_bg_pkmn(cert)
-    print(f"\nCards with the same Pokemon in the background ({pkmn}):")
-    print(same)
+        same, pkmn = collection.find_same_attr(cert, "pkmn")
+        print(f"\nCards with the same Pokemon ({pkmn}):")
+        print(same)
 
-    same, _set = collection.find_same_attr(cert, "set")
-    print(f"\nCards in the same set ({_set}):")
-    print(same)
+        same = collection.find_same_bg_pkmn(cert)
+        print(f"\nCards with the same Pokemon in the background ({pkmn}):")
+        print(same)
+
+        same, _set = collection.find_same_attr(cert, "set")
+        print(f"\nCards in the same set ({_set}):")
+        print(same)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("cert")
+    parser.add_argument("--price", default=False, action="store_true")
     args = parser.parse_args()
-    find(args.cert)
+    find(args.cert, args.price)
